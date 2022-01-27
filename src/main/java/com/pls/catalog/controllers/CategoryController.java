a36a35a0469e962bd4a49161aa934233c904ee32
Pedro-lms <pedrodaluzmachado@gmail.com>
2022-01-26 23:33:45 -0300

    "HTTP verbs, Exceptions Tratment and postgres correct password"

--- src/main/java/com/pls/catalog/controllers/CategoryController.java
+++ src/main/java/com/pls/catalog/controllers/CategoryController.java
@@ -2,26 +2,51 @@ package com.pls.catalog.controllers;
 
 import com.pls.catalog.dto.CategoryDTO;
 import com.pls.catalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories" )
 public class CategoryController {
 
-    private final CategoryService service;
-
-    public CategoryController(CategoryService service) {
-        this.service = service;
-    }
+    @Autowired
+    private CategoryService service;
 
     @GetMapping
     public ResponseEntity<List<CategoryDTO>> findAll(){
         List<CategoryDTO> list = service.findAll(); //Service chama o Repository que chama o find all no banco de dados
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+        CategoryDTO dto = service.findById(id); //Service chama o Repository que chama o find all no banco de dados
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    //Pra que o objeto seja reconhecido e case com o objeto dto, passa-se o @RequestBody
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/pls/catalog/services/CategoryService.java
+++ src/main/java/com/pls/catalog/services/CategoryService.java
@@ -3,11 +3,17 @@ package com.pls.catalog.services;
 import com.pls.catalog.dto.CategoryDTO;
 import com.pls.catalog.models.Category;
 import com.pls.catalog.repositories.CategoryRepository;
+import com.pls.catalog.services.exceptions.ControllerNotFoundException;
+import com.pls.catalog.services.exceptions.DataBaseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,11 +25,46 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll(){
         List<Category> list= repository.findAll();
-
         /**
          * Usando lambda pra pegar cada elemento da lista de Categoria e levando para um elemento da lista CategoriaDTO
          */
         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+    }
 
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new ControllerNotFoundException("Objeto não encontrado"));
+        return new CategoryDTO(entity); //Transforma o objeto da Category com nome obj em CategoryDTO
+    }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+      try {
+          Category entity = repository.getOne(id);
+          entity.setName(dto.getName());
+          entity = repository.save(entity);
+          return new CategoryDTO(entity);
+      }catch (EntityNotFoundException e){
+         throw new ControllerNotFoundException("Id não encontrado" + id);
+      }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ControllerNotFoundException("Id não Encontrado" + id);
+        }catch (DataIntegrityViolationException d) {
+            throw new DataBaseException("Violação de Integridade");
+        }
     }
 }
